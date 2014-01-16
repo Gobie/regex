14b3c0a8d9ecbd1529469b9a93d7c9ea8ad0af4f
Michal Brašna <michal.brasna@gmail.com>
2014-01-16 05:31:58 +0100

    Add RegexFacadeTest

---
+++ tests/Gobie/Test/Regex/RegexFacadeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Gobie\Test\RegexFacade;
+
+use Gobie\Regex\RegexFacade;
+
+class RegexFacadeTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider provideTestShouldCreateRegexWithDriver
+     */
+    public function testShouldCreateRegexWithDriver($driver)
+    {
+        $regex = new RegexFacade($driver);
+        $this->assertSame($regex->getDriverClass(), $driver);
+    }
+
+    public function provideTestShouldCreateRegexWithDriver()
+    {
+        return array(
+            array(RegexFacade::PCRE),
+        );
+    }
+
+    /**
+     * @dataProvider provideTestShouldMatch
+     */
+    public function testShouldMatch($driver, $pattern, $subject, $expectedResult)
+    {
+        $regex = new RegexFacade($driver);
+        $this->assertSame($regex->match($pattern, $subject), $expectedResult);
+    }
+
+    public function provideTestShouldMatch()
+    {
+        return array(
+            array(RegexFacade::PCRE, '/abc/', '01abc23', array('abc')),
+        );
+    }
+}
