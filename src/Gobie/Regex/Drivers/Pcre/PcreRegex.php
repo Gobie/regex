40fa801137c02b0252fb28bcef010d19a4c187a1
Michal Brašna <michal.brasna@gmail.com>
2014-01-17 23:08:00 +0100

    Extract prepare and cleanup methods in PcreRegex

--- src/Gobie/Regex/Drivers/Pcre/PcreRegex.php
+++ src/Gobie/Regex/Drivers/Pcre/PcreRegex.php
@@ -7,55 +7,45 @@ class PcreRegex
 
     public static function match($pattern, $subject)
     {
-        set_error_handler(function ($errno, $errstr) use ($pattern) {
-            restore_error_handler();
-            throw new PcreRegexException($errstr, null, $pattern);
-        });
-
+        self::prepare($pattern);
         $res = preg_match($pattern, $subject);
-
-        restore_error_handler();
-
-        if ($res === false && preg_last_error()) {
-            throw new PcreRegexException(null, preg_last_error(), $pattern);
-        }
+        self::cleanup($pattern);
 
         return (bool) $res;
     }
 
     public static function get($pattern, $subject)
     {
-        set_error_handler(function ($errno, $errstr) use ($pattern) {
-            restore_error_handler();
-            throw new PcreRegexException($errstr, null, $pattern);
-        });
-
+        self::prepare($pattern);
         $res = preg_match($pattern, $subject, $matches);
-
-        restore_error_handler();
-
-        if ($res === false && preg_last_error()) {
-            throw new PcreRegexException(null, preg_last_error(), $pattern);
-        }
+        self::cleanup($pattern);
 
         return $res ? $matches : array();
     }
 
     public static function getAll($pattern, $subject)
     {
+        self::prepare($pattern);
+        $res = preg_match_all($pattern, $subject, $matches);
+        self::cleanup($pattern);
+
+        return $res ? $matches : array();
+    }
+
+    private static function prepare($pattern)
+    {
         set_error_handler(function ($errno, $errstr) use ($pattern) {
             restore_error_handler();
             throw new PcreRegexException($errstr, null, $pattern);
         });
+    }
 
-        $res = preg_match_all($pattern, $subject, $matches);
-
+    private static function cleanup($pattern)
+    {
         restore_error_handler();
 
-        if ($res === false && preg_last_error()) {
+        if (preg_last_error()) {
             throw new PcreRegexException(null, preg_last_error(), $pattern);
         }
-
-        return $res ? $matches : array();
     }
 }
