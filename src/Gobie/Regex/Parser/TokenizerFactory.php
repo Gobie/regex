e8916726b661ad65308c404e3c23e404a038b7e6
Michal Brašna <michal.brasna@gmail.com>
2014-02-11 00:07:04 +0100

    Add docs for TokenizerFactory

--- src/Gobie/Regex/Parser/TokenizerFactory.php
+++ src/Gobie/Regex/Parser/TokenizerFactory.php
@@ -5,6 +5,9 @@ namespace Gobie\Regex\Parser;
 class TokenizerFactory implements TokenizerFactoryInterface
 {
 
+    /**
+     * {@inheritdoc}
+     */
     public function create($data, $encoding = 'UTF-8')
     {
         return new Tokenizer($data, $encoding);
--- src/Gobie/Regex/Parser/TokenizerFactoryInterface.php
+++ src/Gobie/Regex/Parser/TokenizerFactoryInterface.php
@@ -4,5 +4,13 @@ namespace Gobie\Regex\Parser;
 
 interface TokenizerFactoryInterface
 {
+
+    /**
+     * Creates Tokenizer.
+     *
+     * @param string $data     Data to tokenize
+     * @param string $encoding Encoding
+     * @return TokenizerInterface
+     */
     public function create($data, $encoding = 'UTF-8');
 }
